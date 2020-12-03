25f8641dcae3716fbb4034bb042de15679e38f5d
Liliripe <liliripe.studio@gmail.com>
2020-12-03 13:18:58 -0600

    document error test 2

--- content/cane-chair/index.md
+++
@@ -1,9 +0,0 @@
----
-path: '/product/cane-chair'
-cover: './assets/thumbnail.jpg'
-date: '2019-05-08'
-title: 'Cane Chair'
-published: true
-tags: ['furniture', 'chair']
----
-An upscale bistro spirit chair, which revisits the use of caning with modernity and elegance.
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,61 +1,11 @@
-import React, { useEffect } from 'react'
-import { graphql, Link } from 'gatsby'
-import styled from 'styled-components'
-import locomotiveScroll from 'locomotive-scroll'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
+import React from 'react'
 
-import SEO from '../helpers/seo'
-
-const IndexPage = ({ data }) => {
-  const { edges } = data.allMarkdownRemark
-
-  const scrollRef = React.createRef()
-  useEffect(() => {
-    const scroll = new locomotiveScroll({
-      el: scrollRef.current,
-      smooth: true,
-    })
-  })
-
+const IndexPage = () => {
   return (
-    <div className="scroll" ref={scrollRef}>
-      <SEO title="Home" />
+    <div>
       test
     </div>
   )
 }
-
-export const query = graphql`
-  query {
-    allMarkdownRemark(
-      limit: 6
-      sort: { order: ASC, fields: [frontmatter___date] }
-    ) {
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            path
-            tags
-            date(formatString: "MM.DD.YYYY")
-            cover {
-              childImageSharp {
-                fluid(
-                  maxWidth: 1000
-                  quality: 90
-                  traceSVG: { color: "#f2f2f2" }
-                ) {
-                  ...GatsbyImageSharpFluid_withWebp_tracedSVG
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`
 
 export default IndexPage
